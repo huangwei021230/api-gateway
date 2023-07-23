99486c039d06ef6b2846fd06b04126be8aabd501
root <root@Alex-ver1>
2023-07-23 12:21:37 +0800

    feat:unit_test & performance_test

---
+++ test/unit_test/division_test.go
@@ -0,0 +1,26 @@
+package unit_test
+
+import (
+	divition "github.com/huangwei021230/api-gateway/microservices/division-service/kitex_gen/division/management"
+	divImpl "github.com/huangwei021230/api-gateway/test/unit_test/utils"
+	"golang.org/x/net/context"
+	"strconv"
+	"testing"
+)
+
+func Test_division1(t *testing.T) {
+	divisionImpl := new(divImpl.DivisionManagementImpl)
+	req := &divition.DivisionRequest{
+		FirstNum:  "100",
+		SecondNum: "20",
+	}
+	resp, err := divisionImpl.DivideNumbers(context.Background(), req)
+	if err != nil {
+		t.Error(err)
+	}
+	expectResult := 100.00 / 20
+	result, _ := strconv.ParseFloat(resp.Quotient, 64)
+	if result != expectResult {
+		t.Errorf("expect result is %v, but get %v", expectResult, resp.String())
+	}
+}
---
+++ test/unit_test/multiplication_test.go
@@ -0,0 +1,26 @@
+package unit_test
+
+import (
+	multiplication "github.com/huangwei021230/api-gateway/microservices/multiplication-service/kitex_gen/multiplication/management"
+	mulImpl "github.com/huangwei021230/api-gateway/test/unit_test/utils"
+	"golang.org/x/net/context"
+
+	"strconv"
+	"testing"
+)
+
+func Test_multiplication1(t *testing.T) {
+	multiplicationImpl := new(mulImpl.MultiplicationManagementImpl)
+	req := &multiplication.MultiplicationRequest{
+		FirstNum:  "100",
+		SecondNum: "20",
+	}
+	resp, err := multiplicationImpl.MultiplyNumbers(context.Background(), req)
+	if err != nil {
+		t.Error(err)
+	}
+	expectResult := strconv.Itoa(100 * 20)
+	if resp.Product != expectResult {
+		t.Errorf("expect result is %v, but get %v", expectResult, resp.String())
+	}
+}

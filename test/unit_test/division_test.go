15981a93bf85fae39f07af05f36fa2de70ce5c88
root <root@Alex-ver1>
2023-07-23 23:23:23 +0800

    feat: add some test cases

--- test/unit_test/division_test.go
+++ test/unit_test/division_test.go
@@ -24,3 +24,20 @@ func Test_division1(t *testing.T) {
 		t.Errorf("expect result is %v, but get %v", expectResult, resp.String())
 	}
 }
+
+func Test_division2(t *testing.T) {
+	divisionImpl := new(divImpl.DivisionManagementImpl)
+	req := &divition.DivisionRequest{
+		FirstNum:  "0",
+		SecondNum: "20",
+	}
+	resp, err := divisionImpl.DivideNumbers(context.Background(), req)
+	if err != nil {
+		t.Error(err)
+	}
+	expectResult := 0.00 / 20
+	result, _ := strconv.ParseFloat(resp.Quotient, 64)
+	if result != expectResult {
+		t.Errorf("expect result is %v, but get %v", expectResult, resp.String())
+	}
+}
--- test/unit_test/multiplication_test.go
+++ test/unit_test/multiplication_test.go
@@ -24,3 +24,19 @@ func Test_multiplication1(t *testing.T) {
 		t.Errorf("expect result is %v, but get %v", expectResult, resp.String())
 	}
 }
+
+func Test_multiplication2(t *testing.T) {
+	multiplicationImpl := new(mulImpl.MultiplicationManagementImpl)
+	req := &multiplication.MultiplicationRequest{
+		FirstNum:  "0",
+		SecondNum: "20",
+	}
+	resp, err := multiplicationImpl.MultiplyNumbers(context.Background(), req)
+	if err != nil {
+		t.Error(err)
+	}
+	expectResult := strconv.Itoa(0 * 20)
+	if resp.Product != expectResult {
+		t.Errorf("expect result is %v, but get %v", expectResult, resp.String())
+	}
+}
